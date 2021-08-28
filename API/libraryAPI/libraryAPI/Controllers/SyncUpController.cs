ced243f21297bf362019c361c5ce3740eb990ccb
Andrea Pulido <puli1515@gmail.com>
2021-08-28 00:07:38 -0500

    first commit

---
+++ API/libraryAPI/libraryAPI/Controllers/SyncUpController.cs
@@ -0,0 +1,32 @@
+﻿using libraryAPI.BLL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SyncUpController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            SyncUpBLL syncUp = new SyncUpBLL();
+            bool response = syncUp.Start();
+            if (response)
+            {
+                return StatusCode(StatusCodes.Status200OK);
+
+            }
+            else 
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+
+        }
+    }
+}
---
+++ API/libraryAPI/libraryAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using libraryAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace libraryAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Authors> Authors { get; set; }
+        public DbSet<Books> Books { get; set; }
+    }
+}

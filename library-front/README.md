e06bb62a02cf2d99d351c3ca809928cf09f97809
Andrea Pulido <puli1515@gmail.com>
2021-08-28 09:49:38 -0500

    creando de nuevo el front

--- library-front/README.md
+++ library-front/README.md
@@ -1,4 +1,4 @@
-# Library
+# LibraryFront
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
--- library-front/src/app/app.component.spec.ts
+++ library-front/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'library'`, () => {
+  it(`should have as title 'library-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('library');
+    expect(app.title).toEqual('library-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('library app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('library-front app is running!');
   });
 });
